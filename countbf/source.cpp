9ccf372c03a0f42a764649f68b98c35e738ce2b3
Daniil <plyushenko@bk.ru>
2016-05-31 02:06:25 +0300

    upgraded hashing

--- countbf/source.cpp
+++ countbf/source.cpp
@@ -28,32 +28,33 @@ void bin(uint64_t x)
 		cerr << ans[i];
 	}
 	cerr << "\n";
-}     
+} 
+
+template <class T> uint64_t double_hashing(const T&, uint32_t);
+
+template <class T>    
 class BloomFilter
 {
-	private:
-		uint32_t
+		private:
+				uint32_t
 					bits_per_counter, //should be divisor of size_of_cell (which is 64)
 					counters_per_cell,
 					counters_per_element,
 					bits_per_element,
-					number_of_hashes,
+					number_of_hashes;
+				uint64_t
 					capacity,
 					mod,
 					length;
 				uint64_t max_counter;
-                atomic <uint64_t> *data;
+				atomic <uint64_t> *data;
+				uint64_t (*get_hash)(const T&, uint32_t);
 
-				int32_t get_hash(const char *s, uint32_t i) const
-				{
-					return CityHash64WithSeed(s, strlen(s), i) % mod;
-//					return ((1234 + i) * s[0]) % mod;
-				}
-				uint32_t get_index(uint32_t x) const
+				uint64_t get_index(uint64_t x) const
 				{
 					return x / counters_per_cell;
 				}
-				uint32_t get_offset(uint32_t x) const
+				uint32_t get_offset(uint64_t x) const
 				{
 					return ((x % counters_per_cell) + 1) * bits_per_counter;
 				}
@@ -63,7 +64,7 @@ class BloomFilter
 				}
 				uint64_t increment(uint64_t x)
 				{
-					return x += (x != (1ull << bits_per_counter));
+					return x += (x != max_counter);
 				}   	
 				uint64_t decrement(uint64_t x)
 				{
@@ -73,10 +74,12 @@ class BloomFilter
 				{
 					uint32_t shift = size_of_cell - offset + bits_per_counter;
 					uint64_t old = (shift == size_of_cell) ? 0 : ((value >> shift) << shift);
-					return old | (new_counter << (shift - bits_per_counter)) | value % (1ull << (shift - bits_per_counter));
+					return old | (new_counter << (shift - bits_per_counter)) | (value % (1ull << (shift - bits_per_counter)));
 				}
+
 		public:
-			BloomFilter(uint32_t my_capacity, uint32_t my_bpc, uint32_t my_cpe):
+			BloomFilter(uint64_t my_capacity, uint32_t my_bpc, uint32_t my_cpe, 
+						uint64_t (*my_hash_family)(const T&, uint32_t) = &double_hashing<T>):
 				bits_per_counter(my_bpc),
 				counters_per_cell(size_of_cell / bits_per_counter),
 				counters_per_element(my_cpe),
@@ -85,14 +88,20 @@ class BloomFilter
 				mod(my_capacity),
 				length((my_capacity + counters_per_cell - 1) / counters_per_cell),
 				max_counter((1ull << bits_per_counter) - 1),
-				data(new atomic <uint64_t>[length]){fill(data, data + length, 0);}//{cerr << "length = " << length << "\n";}
+				data(new atomic <uint64_t>[length]),
+				get_hash(my_hash_family)
+				{
+					fill(data, data + length, 0);
+					if (get_hash == &double_hashing<T>)
+						cout << "\nWARNING: hash family hasn't been provided!\nUsing double-hashing based on std::hash\n\n";
+				}
 
-			void insert(const char *s)
+			void insert(const T &value)
 			{
 				for (uint32_t i = 0; i < number_of_hashes; i++)
 				{
-					uint32_t h = get_hash(s, i);
-					uint32_t index = get_index(h);				
+					uint64_t h = get_hash(value, i) % mod;
+					uint64_t index = get_index(h);				
 					uint32_t offset = get_offset(h);
 					uint64_t new_counter = increment(get_counter(data[index], offset));
 					uint64_t value = data[index];
@@ -106,12 +115,13 @@ class BloomFilter
 					while (1);
 				}
 			}
-			void erase(const char *s)
+
+			void erase(const T& value)
 			{
 				for (uint32_t i = 0; i < number_of_hashes; i++)
 				{
-					uint32_t h = get_hash(s, i);
-					uint32_t index = get_index(h);				
+					uint64_t h = get_hash(value, i) % mod;
+					uint64_t index = get_index(h);				
 					uint32_t offset = get_offset(h);
 					uint64_t new_counter = decrement(get_counter(data[index], offset));
 					uint64_t value = data[index];
@@ -125,50 +135,61 @@ class BloomFilter
 					while (1);
 				}
 			}
-			uint64_t count(const char *s) const
+
+			uint64_t count(const T& value) const
 			{
-				uint64_t result = 1 << bits_per_counter;
+				uint64_t result = max_counter;
 				for (uint32_t i = 0; i < number_of_hashes; i++)
 				{
-					uint32_t h = get_hash(s, i);
-   					uint32_t index = get_index(h);				
+					uint64_t h = get_hash(value, i) % mod;
+   					uint64_t index = get_index(h);				
 					uint32_t offset = get_offset(h);
 					uint64_t counter = get_counter(data[index], offset);
-
 					result = min(result, counter);
 				}
 				return result;
 			} 
-			
 };
+
+template <class T>
+uint64_t double_hashing(const T& value, uint32_t i)
+{
+	//hash1 is std::hash and hash2 is just polynom with magic prime numbers
+	return (hash<T>()(value) + 31013 * i * i + 1117711 * i);
+}
+
+uint64_t const_test(const char * const &value, uint32_t i)
+{
+	return CityHash64WithSeed(value, strlen(value), i);
+}
+
+uint64_t test(char * const &value, uint32_t i)
+{
+	return CityHash64WithSeed(value, strlen(value), i);
+}
+
 int main()
 {
     freopen("cerr", "w", stderr);
-	BloomFilter bf(1e3, 4, 3);
-	for (int i = 0; i < 26; i++)
-	{
-	    char s[1];
-	    s[0] = i + 'a';
-	    cerr << i << " " << s[0] << "\n";
-	    cout << i << "\n";
-	    bf.insert(s);
-	}
-	for (int i = 0; i < 26; i++)
-	{
-	    char s[1];
-	    s[0] = i + 'a';
-	    cerr << i << " " << s[0] << "\n";
-	    cout << i << "\n";
-	    bf.erase(s);
-	}
-		
-	for (int i = 0; i < 26; i++)
-	{
-	    char s[1];
-	    s[0] = i + 'a';
-	    cerr << i << " " << s[0] << "\n";
-	    cout << bf.count(s) << "\n";
-	}	
 
-//	cerr << bf.count("a");
+	BloomFilter <string> bf(1e3, 4, 3);
+	for (int i = 0; i < 10; i++)
+		bf.insert("aaaaa");
+	bf.erase("aaaaa");
+	for (int i = 0; i < 5; i++)
+	{
+		string tmp; tmp += char('A' + 3 * i);		
+		bf.insert(tmp);
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		string tmp; tmp += char('A' + i);
+		cout << bf.count(tmp) << " ";
+	}
+	cout << bf.count("aaaaa") << "\n";
+	
+	BloomFilter <char *> bf2(1e3, 4, 3, test);
+	BloomFilter <const char *> bf3(1e3, 4, 3, const_test);
+	
+	return 0;
 }
